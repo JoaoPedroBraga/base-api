94dfb065f61976aec599a77504a353fc7270cf2f
joaopedrocostabraga17 <joaopedrocostabraga17@gmail.com>
2024-02-20 18:54:00 +0000

    Adicionando rotas de cadastro, edição e delete

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -19,4 +19,36 @@ getOneClient = (req, res) => {
     }
 }
 
-module.exports = { getClients, getOneClient };
+creatClient = (req, res) =>{
+    const cliente = req.body
+    if(Object.keys(cliente).length){
+        clientes.push(cliente);
+        res.status(200).send(cliente)
+    }else{
+        res.status(406).send('Insira o dados do Cliente')
+    }
+}
+
+updateCliente = (req, res) =>{
+    let id = req.params.id;
+    let indice = findClientIndex(id);
+    clientes[indice] = req.body;
+    res.status(200).send('Cliente Atualizado')
+}
+
+removeClient = (req, res) => {
+    let id = req.params.id;
+    let indice = findClientIndex(id);
+    clientes.splice(indice, 1)
+
+    res.status(200).send(`O id: ${id} foi excluido`)
+}
+
+//Função para puxar o cliente para edição
+findClientIndex = (id) => {
+    //                      findIndex retorna o index do ID
+    const indice = clientes.findIndex((item) => item.id === Number(id));
+    return indice
+}
+
+module.exports = { getClients, getOneClient, creatClient, updateCliente, removeClient };
--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const clientController = require('./../controllers/clientController')
 
 router.get('/', clientController.getClients);
-router.get('/:id', clientController.getOneClient)
+router.get('/:id', clientController.getOneClient);
+router.post('/', clientController.creatClient);
+router.put('/:id', clientController.updateCliente);
+router.delete('/:id', clientController.removeClient)
 
 module.exports = router;
